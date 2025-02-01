9e4dc0805994c1ffdc038614cf0c5401e5621a16
Oksana Stepanenko <stepanencode@gmail.com>
2025-02-01 11:44:57 +0100

    WIP: header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './App.scss';
+import planetIcon from './assets/images/planet.svg';
 
 const App = () => {
   return (
-    <div className="app__container">
-      <h1 className="app__title">React App</h1>
-      <p className="app__text">SOFTSWISS.</p>
+    <div className="app">
+      <header className="header">
+        <div className="header__logo">
+          <img src={planetIcon} alt="Logo" className="header__logo-icon" />
+          <span className="header__logo-text">GO</span>
+        </div>
+        <input
+          type="checkbox"
+          id="menu-toggle"
+          className="header__menu-toggle"
+        />
+        <label htmlFor="menu-toggle" className="header__menu-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <nav className="header__nav">
+          <a href="#" className="header__nav-item">
+            Home
+          </a>
+          <a href="#" className="header__nav-item">
+            Products
+          </a>
+          <a href="#" className="header__nav-item">
+            Cart
+          </a>
+        </nav>
+      </header>
     </div>
   );
 };
