99029c8b22250b349cf54f66a0cbf427342fb00f
Oksana Stepanenko <stepanencode@gmail.com>
2025-02-01 07:13:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app__container">
+      <h1 className="app__title">React App</h1>
+      <p className="app__text">SOFTSWISS.</p>
+    </div>
+  );
+};
+
+export default App;
