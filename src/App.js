6f86f1eedba41dd8b54a6ea6f2f2db7c67959855
Oksana Stepanenko <stepanencode@gmail.com>
2025-02-02 10:00:02 +0100

    WIP: semantic

--- src/App.js
+++ src/App.js
@@ -12,28 +12,26 @@ const App = () => {
           <img src={planetIcon} alt="Logo" className="header__logo-icon" />
           <span className="header__logo-text">GO</span>
         </div>
-        <input
-          type="checkbox"
-          id="menu-toggle"
-          className="header__menu-toggle"
-        />
-        <label htmlFor="menu-toggle" className="header__menu-icon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-        <nav className="header__nav">
-          <a href="#" className="header__nav-item">
-            <span className="header__nav-text">Home</span>
-          </a>
-          <a href="#" className="header__nav-item">
-            <span className="header__nav-text">Products</span>
-          </a>
-          <a href="#" className="header__nav-item header__cart">
-            <span className="header__cart-text">Cart</span>
-            <img src={cartIcon} alt="Cart" className="header__cart-icon" />
-          </a>
-        </nav>
+        <div className="menu">
+          <input type="checkbox" id="menu-toggle" className="menu__toggle" />
+          <label htmlFor="menu-toggle" className="menu__icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          <nav className="header__nav">
+            <a href="#" className="header__nav-item">
+              <span className="header__nav-text">Home</span>
+            </a>
+            <a href="#" className="header__nav-item">
+              <span className="header__nav-text">Products</span>
+            </a>
+            <a href="#" className="header__nav-item header__cart">
+              <span className="header__cart-text">Cart</span>
+              <img src={cartIcon} alt="Cart" className="header__cart-icon" />
+            </a>
+          </nav>
+        </div>
       </header>
     </div>
   );
